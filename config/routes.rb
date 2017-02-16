b4840de5dbd111d1ebf566cc9a934557cc746449
菅沼 嘉一 <suganoo2002@gmail.com>
2017-02-16 15:03:27 +0000

    update login files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #get 'welcome/index'
   root to: 'welcome#index'
-
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
