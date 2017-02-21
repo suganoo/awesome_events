7041e1e67dd54d35d59ea3f90e70beeefa443cf1
suganoo <suganoo2002@gmail.com>
2017-02-21 05:18:35 +0000

    update search to ransack

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   PER = 10
 
   def index
-    @q = Event.page(params[:page]).per(PER).order(:start_time).search(search_params)
+    @q = Event.page(params[:page]).per(PER).order(:start_time).ransack(search_params)
     @events = @q.result(distinct: true)
   end
 
